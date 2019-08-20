aaa9d4fb7b23733ba1cd020ad8a6622e1bd2f1d1
nishuia7788 <230739974@qq.com>
2019-08-20 09:27:10 +0800

    '账号静态页面'

---
+++ src/views/acountlist/learn.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>学习卡</div>
+</template>
--- src/views/myacount.vue
+++ src/views/myacount.vue
@@ -1,5 +1,126 @@
 <template >
-  <div>
-    <h1>这是我的账号</h1>
+  <div class="cq_wrap">
+    <!-- <h1>这是我的账号</h1> -->
+    <div class="cq_header">
+      <p>
+        <img src="../../public/images/pagou.jpg" />
+      </p>
+      <dl>
+        <dt>一曲入魂</dt>
+        <dd>正在使用QQ登录</dd>
+      </dl>
+      <b class="iconfont">&#xe602;</b>
+    </div>
+    <div class="cq_main">
+      <ul>
+        <li v-for="item in list" :key="item.id">
+          <!-- <router-link :to="item.path"> -->
+          <span class="iconfont" :class="item.icons"></span>
+          <p>{{item.text}}</p>
+          <b class="iconfont">&#xe602;</b>
+          <!-- </router-link> -->
+        </li>
+      </ul>
+      <div class="cq_last">
+        <span class="iconfont">&#xe7bd;</span>
+        <p>分享赚学费</p>
+        <b class="iconfont">&#xe602;</b>
+      </div>
+    </div>
   </div>
 </template>
+<script>
+export default {
+  // name: 'myacount',
+  // components: {
+  //   Tabbar
+  // },
+  data() {
+    return {
+      list: [
+        { id: 1, text: '我的订单', icons: 'icon-place-order' },
+        { id: 2, text: '我的优惠券', icons: 'icon-youhuiquan' },
+        { id: 3, text: '我的购物车', icons: 'icon-tab_car' },
+        { id: 4, text: '我的学习卡', icons: 'icon-xuexiqia' }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.wrap {
+  display: flex;
+  flex-direction: column;
+  background: #eee;
+}
+.cq_header {
+  background: #fff;
+  height: 0.5rem;
+  padding: 14px 0 14px 12px;
+  display: flex;
+  justify-content: space-between;
+  p {
+    width: 0.5rem;
+    height: 0.5rem;
+    padding-right: 0.12rem;
+    img {
+      width: 100%;
+      border-radius: 0.25rem;
+    }
+  }
+  dl {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    flex: 1;
+    dt {
+      font-size: 0.2rem;
+      color: #333;
+    }
+    dd {
+      color: #999;
+    }
+  }
+  b {
+    align-self: center;
+    margin-right: 0.1rem;
+    font-size: 0.16rem;
+    color: #ccc;
+  }
+}
+.cq_main {
+  flex: 1;
+  overflow-y: auto;
+
+  ul {
+    background: #fff;
+    margin-bottom: 0.1rem;
+  }
+  .cq_last,
+  li {
+    background: #fff;
+    height: 0.5rem;
+    display: flex;
+    align-items: center;
+    font-size: 0.16rem;
+    span {
+      width: 0.42rem;
+      text-align: center;
+      color: #888;
+    }
+
+    p {
+      flex: 1;
+      font-size: 0.16rem;
+    }
+    b {
+      margin-right: 0.1rem;
+      color: #ccc;
+    }
+  }
+  ul > li {
+    border-bottom: 1px solid #ddd;
+  }
+}
+</style>
