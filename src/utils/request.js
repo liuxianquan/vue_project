3c0d8d6ce1762948182ff71c0c7122348a9aa10b
liuxianquan <254735221@qq.com>
2019-08-22 21:54:46 +0800

    修改axios请求头

--- src/utils/request.js
+++ src/utils/request.js
@@ -10,6 +10,14 @@ const request = axios.create({
 })
 
 // 3. 在这还可以实现一些我们的拦截功能
+// 请求拦截
+request.interceptors.request.use(config => {
+  // 将token携带在请求头上。
+  if (store.state.user.token) {
+    config.headers['Access_Token'] = store.state.user.token
+  }
+  return config
+})
 
 // 3.1 响应拦截
 //    3.1.1 直接将 response.data 给返回出去，以方便外面调用时还得 .data
